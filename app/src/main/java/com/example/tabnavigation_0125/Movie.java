afd8d8a5280c59859c67cdcba0ea6eaab6082c3c
uttunggahw <uttungga.w@students.amikom.ac.id>
2020-11-29 19:50:13 +0700

    Praktek Navigation

---
+++ app/src/main/java/com/example/tabnavigation_0125/Movie.java
@@ -0,0 +1,26 @@
+package com.example.tabnavigation_0125;
+
+public class Movie {
+    private String title, year;
+
+    public Movie(String title, String year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
